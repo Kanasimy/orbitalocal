a61ec57fceb34bcf7f3e32d7cbaf0bae545ebf5b
Kanasimy <admin@active-way.ru>
2021-12-16 16:42:01 +0700

    add video in the catalog.element

--- templates/orbitansk/components/bitrix/catalog.element/.default/result_modifier.php
+++ templates/orbitansk/components/bitrix/catalog.element/.default/result_modifier.php
@@ -7,3 +7,26 @@
 
 $component = $this->getComponent();
 $arParams = $component->applyTemplateModifications();
+
+
+// additional video
+$arResult["VIDEO"] = array();
+if (isset($arResult["PROPERTIES"]["FILES"]["VALUE"]) && is_array($arResult["PROPERTIES"]["FILES"]["VALUE"])) {
+    foreach ($arResult["PROPERTIES"]["FILES"]["VALUE"] as $FILE) {
+        $FILE = CFile::GetFileArray($FILE);
+        if (is_array($FILE) && $FILE["CONTENT_TYPE"] === "video/mp4")
+            $arResult["VIDEO"][] = $FILE;
+    }
+}
+
+// additional photo
+$arResult["MORE_PHOTO"] = array();
+if (isset($arResult["PROPERTIES"]["MORE_PHOTO"]["VALUE"]) && is_array($arResult["PROPERTIES"]["MORE_PHOTO"]["VALUE"])) {
+    foreach ($arResult["PROPERTIES"]["MORE_PHOTO"]["VALUE"] as $FILE) {
+        $FILE = CFile::GetFileArray($FILE);
+        if (is_array($FILE))
+            $arResult["MORE_PHOTO"][] = $FILE;
+    }
+}
+
+$arResult['MORE_PHOTO_COUNT'] = sizeof( $arResult['MORE_PHOTO'] );
